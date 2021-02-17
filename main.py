5c018d16f1ee7d25271a4c5263b84eb0f1cff2a1
Chris Warrilow <meth0d@comcast.net>
2021-02-17 06:04:55 +0000

    message

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ daysRemaining = yearsRemaining * 365
 weeksRemaning = yearsRemaining * 52
 monthsRemaining = yearsRemaining * 12
 
-print(f"You have {daysRemaining} days remaining or {monthsRemaining} months remaining or {yearsRemaining} years remaining until you are 90 years old.")
+message = (f"You have {daysRemaining} days remaining or {monthsRemaining} months remaining or {yearsRemaining} years remaining until you are 90 years old.")
+
+print(message)
